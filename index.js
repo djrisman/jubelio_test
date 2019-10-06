6669cbace82811da9b8c77fbc3af6c880c8824a7
Risman <2190636@transcosmos-id.local>
2019-10-07 05:58:01 +0700

    test_revision

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const db = require('./queries')
 const Hapi = require('hapi');
+const axios = require('axios');
+var parser = require('fast-xml-parser');
+var he = require('he');
 
 const init = async () => {
 
@@ -9,6 +12,26 @@ const init = async () => {
     });
 
     await server.register(require('inert'));
+
+    var options = {
+    attributeNamePrefix : "@_",
+    attrNodeName: "attr", //default is 'false'
+    textNodeName : "#text",
+    ignoreAttributes : true,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : false,
+    parseNodeValue : true,
+    parseAttributeValue : false,
+    trimValues: true,
+    cdataTagName: "__cdata", //default is 'false'
+    cdataPositionChar: "\\c",
+    localeRange: "", //To support non english character in tag/attribute values.
+    parseTrueNumberOnly: false,
+    attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+    tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+    stopNodes: ["parse-me-as-string"]
+    };
+ 
 
     server.route({
       method:'GET',
@@ -23,6 +46,75 @@ const init = async () => {
           console.log({"code":200, "message":"Bismillah, NodeJS and HapiJS API"});
           return {"code":200, "message":"Bismillah, NodeJS and HapiJS API"};
           //return h.file('upload.html');
+      }
+    });
+
+    server.route({
+      method:'GET',
+      path:'/tesapi',
+      config: {
+          cors: {
+              origin: ['*'],
+              additionalHeaders: ['cache-control', 'x-requested-with']
+          }
+      },
+      handler: function(request, h){
+
+        return axios.get('http://api.elevenia.co.id/rest/prodservices/product/listing', {
+                   headers: {
+                      'Content-type' : 'application/xml',
+                      'Accept-Charset' : 'utf-8',
+                      openapikey: '721407f393e84a28593374cc2b347a98'
+                   }
+                  })
+                  .then((response) => {
+                    console.log(response.data, "dattt");
+                    var tObj = parser.getTraversalObj(response.data,options);
+                    var jsonObj = parser.convertToJson(tObj,options);
+                                    
+                    return jsonObj.Products.product;
+
+                  }).catch((e) => {
+                    //console.error(e.stack);
+                    return e;
+                  });
+
+        //return 'adakah acara?';
+          
+      }
+    });
+
+    server.route({
+      method:'GET',
+      path:'/prod_detail/{id_prod}',
+      config: {
+          cors: {
+              origin: ['*'],
+              additionalHeaders: ['cache-control', 'x-requested-with']
+          }
+      },
+      handler: function(request, h){
+        const id = parseInt(request.params.id_prod);
+        
+        return axios.get(`http://api.elevenia.co.id/rest/prodservices/product/details/${id}`, {
+                   headers: {
+                      'Content-type' : 'application/xml',
+                      'Accept-Charset' : 'utf-8',
+                      openapikey: '721407f393e84a28593374cc2b347a98'
+                   }
+                  })
+                  .then((response) => {
+                    //console.log(response.data, "det");
+                    var tObj = parser.getTraversalObj(response.data,options);
+                    var jsonObj = parser.convertToJson(tObj,options);
+                    
+                    return jsonObj.Product;
+
+                  }).catch((e) => {
+                    //console.error(e.stack);
+                    return e;
+                  });
+          
       }
     });
 
